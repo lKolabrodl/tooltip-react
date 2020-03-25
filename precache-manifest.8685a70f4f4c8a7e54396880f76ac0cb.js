9d1a790660744257a1e3fa77a516c25bc4a10405
Bril Mikhail <Kolabrod@mail.ru>
2020-03-25 18:56:40 +0300

    Updates

---
+++ precache-manifest.8685a70f4f4c8a7e54396880f76ac0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833d22cc9445c39fada1158476e06707",
+    "url": "/tooltip-react/index.html"
+  },
+  {
+    "revision": "0ba94198ba16df1cc4cd",
+    "url": "/tooltip-react/static/css/main.14669567.chunk.css"
+  },
+  {
+    "revision": "0818d3b8e45d62fbf517",
+    "url": "/tooltip-react/static/js/2.e76a135d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip-react/static/js/2.e76a135d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba94198ba16df1cc4cd",
+    "url": "/tooltip-react/static/js/main.f81809d4.chunk.js"
+  },
+  {
+    "revision": "7d6d7836add9a141c54a",
+    "url": "/tooltip-react/static/js/runtime-main.3c63aa4b.js"
+  }
+]);
